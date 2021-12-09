d0c41598480a837e6445ccca5243caacdeeda532
Oscar Gürtler <oscarguertler@gmail.com>
2021-12-09 19:27:29 +0100

    add runner for contract

--- scripts/run.js
+++ scripts/run.js
@@ -6,6 +6,12 @@ const main = async () => {
 
 const runMain = async () => {
     try {
-        
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
     }
-}
+};
+
+runMain();
