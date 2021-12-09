feccf13c1480c0f54484bc2ea0432d7cc0087afd
Oscar Gürtler <oscarguertler@gmail.com>
2021-12-09 19:33:27 +0100

    implement wave logic

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,18 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const waveContract = await waveContractFactory.deploy();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
